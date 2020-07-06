7c21ca6c6e686342b4c8815672ef955ac959c21d
daniel.salifu <daniel.salifu@ubuntuafrika.com>
2020-07-06 15:49:55 +0000

    CREATED: Samura components with styling

---
+++ src/app/samura/samura.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-samura',
+  templateUrl: './samura.component.html',
+  styleUrls: ['./samura.component.css']
+})
+export class SamuraComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
