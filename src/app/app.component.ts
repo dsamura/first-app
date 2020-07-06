e85f3a4d7ba36d398cd66c547a60a6f2801c8d39
daniel.salifu <daniel.salifu@ubuntuafrika.com>
2020-07-06 15:50:12 +0000

    CREATED: Samura components with styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   templateUrl: './app.component.html',
+   // styleUrls: ['./app.component.css']
+  styles:  [`
+  h3 {
+    color: blue;
+  }`]
 })
+
 export class AppComponent {
-  title = 'first-app';
-}
+
+  }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SamuraComponent } from './samura/samura.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    SamuraComponent
   ],
   imports: [
     BrowserModule
